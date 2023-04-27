8f32390a7e0a25876916db118b8a60493338ec8c
Nago-Lopez <123041779+Nago-Lopez@users.noreply.github.com>
2023-04-26 21:22:37 -0600

    Add files via upload

---
+++ Parcial_3_AbdenagoLopez/Parcial_3_AbdenagoLopez/src/parcial_3_abdenagolopez/Parcial_3_AbdenagoLopez.java
@@ -0,0 +1,18 @@
+
+package parcial_3_abdenagolopez;
+
+
+public class Parcial_3_AbdenagoLopez {
+
+ 
+    public static void main(String[] args) {
+     
+      control_de_inventario inicio = new control_de_inventario();
+     
+       inicio.setVisible(true);
+        
+        
+        
+    }
+    
+}
